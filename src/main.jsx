cf772fe40e3923b6e5deaf5548305e0eb0d4712d
comunipro2026-oss <comunipro2026@gmail.com>
2026-06-01 01:02:57 -0300

    Update import path for HadrionApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HadrionApp from './App.jsx'
+import HadrionApp from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
